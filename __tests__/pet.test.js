28fd7712c188fd512c55cc22d9f1632622fbe8bf
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-15 21:14:33 +0000

    add hunger and fitness props and tests

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,3 +5,43 @@ describe("constructor", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
 });
+
+describe("constructor", () => {
+  it("sets the name property", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.name).toEqual("Fido");
+  });
+});
+
+describe("constructor", () => {
+  it("has a initial age of 0", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.age).toEqual(0);
+  });
+});
+
+describe("growUp", () => {
+  it("increments the age by 1", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+  it("increments the hunger by 5", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+  it("decrements the fitness by 3", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  });
+});
